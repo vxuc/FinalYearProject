fad7e00d458a86f3dd702051615bb925d6442d29
Jordan <jawdanlow@gmail.com>
2022-09-12 13:34:35 +0800

    added viewcam scene

---
+++ FinalYearProject/Assets/Project/Scripts/ViewingGallery/ViewCamera.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewCamera : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] Transform target;
+    [SerializeField] float distanceToTarget = 10;
+    [SerializeField] [Range(0, 360)] int maxRotationInOneSwipe = 180;
+    [SerializeField] [Range(0, 100)] float zoomSpeed = 10;
+
+    Vector3 previousPosition;
+    [SerializeField] Vector3 offset;
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            Vector3 newPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+            Vector3 direction = previousPosition - newPosition;
+
+            float rotationAroundYAxis = -direction.x * maxRotationInOneSwipe; // camera moves horizontally
+            float rotationAroundXAxis = direction.y * maxRotationInOneSwipe; // camera moves vertically
+
+            cam.transform.position = target.position + offset;
+
+            cam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
+            cam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World); // <� This is what makes it work!
+
+            cam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+
+            previousPosition = newPosition;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            cam.fieldOfView += zoomSpeed;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            cam.fieldOfView -= zoomSpeed;
+        }
+
+        cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, 5f, 100f);
+    }
+}
