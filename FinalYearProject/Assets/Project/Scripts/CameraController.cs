5849e435de162d4d15f5e0b5df6b782fe3368014
Mark Albert Carreon <mark.albert1995@gmail.com>
2022-11-16 15:42:31 +0800

    Chinook

--- FinalYearProject/Assets/Project/Scripts/CameraController.cs
+++ FinalYearProject/Assets/Project/Scripts/CameraController.cs
@@ -398,7 +398,7 @@ public class CameraController : MonoBehaviour
 
         if (objectGazedTracked?.transform.Find("Pivot")) //Get the supposed area that need to be tracked
         {
-            Vector3 toRotate = objectGazedTracked.transform.Find("Pivot").position * objectGazedTracked.GetComponentInParent<PlaneMovement>().movementSpeed / 10080
+            Vector3 toRotate = objectGazedTracked.transform.Find("Pivot").position - objectGazedTracked.transform.Find("Pivot").localPosition*100
                 - transform.position;
 
             //Vector3 toRotate = objectGazedTracked.transform.position -
